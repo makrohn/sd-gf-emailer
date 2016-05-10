4a063a7c5e43da3018b9af23283b2eda4cbc5d33
Matthew Krohn <makrohn@gmail.com>
2016-05-10 15:49:26 -0400

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # service_desk_survey_sender
 Sends an email from Google Forms to requestor on recently closed JIRA tickets
+
+# Setup
+1. Create a Google form, with a field called "Ticket Number"
+2. View the live Google form, and find the html for the ticket number field. Copy the entryid
+3. Create a user with permissions to access the JIRA project via API
+4. Make sure the default screen for the project includes the "Labels" field
+5. Copy settings.py.sample to settings.py
+6. Populate the settings.py with appropriate values
+7. Put somewhere and attach it to a periodic cronjob
---
+++ feedbackRequestor.py
@@ -0,0 +1,69 @@
+"""
+A program to find recently-closed JIRA tickets and email
+a google form to request feedback.
+"""
+
+import json
+import requests
+import smtplib
+from email.mime.text import MIMEText
+import settings
+
+
+def getIssuesFromToday():
+    """Fetch a JSON file of issues according to filter"""
+    r = requests.get(
+        settings.api_base_url + "/search?jql=" + settings.search_filter +
+        "&fields=" + settings.fields,
+        auth=(
+            settings.username,
+            settings.password
+            )
+        )
+    return r.json()
+
+
+def sendSurvey(issue):
+    """Send a an email to the reporter of an issue"""
+    msg = MIMEText(
+        'Hello!\n\n The Help Desk has recently closed your ticket, "' +
+        issue['fields']['summary'] + '". If you have a moment, please fill out ' +
+        ' this survey on how happy you are with your Help Desk experience.\n\n' +
+        settings.google_form_link + '?entry.' +
+        'settings.ticket_number_field' + '=' +
+        issue['key'] + '\n\nThanks!\nThe Help Desk Staff'
+        )
+    msg['Subject'] = "Help Desk Survey for Ticket " + issue['key']
+    msg['From'] = settings.from_address
+    msg['To'] = issue['fields']['reporter']['emailAddress']
+    s = smtplib.SMTP(settings.smtp_server)
+    s.sendmail(
+        settings.from_address,
+        ['mkrohn@dyn.com', issue['fields']['reporter']['emailAddress']],
+        msg.as_string()
+        )
+    print msg
+
+
+def addLabel(issue_key):
+    """Add a label indicating that a survey has been sent to the requestor"""
+    json_data = json.dumps({"update": {"labels": [{"add": "survey_sent"}]}})
+    headers = {'Content-Type': 'application/json', }
+    r = requests.put(
+        url=settings.api_base_url + '/issue/' + issue_key,
+        data=json_data,
+        auth=(
+            settings.username,
+            settings.password
+            ),
+        headers=headers
+        )
+    print r.status_code
+    print r.text
+
+todaysIssues = getIssuesFromToday()
+
+for ticket in todaysIssues['issues']:
+    if 'survey_sent' not in ticket['fields']['labels']:
+        sendSurvey(ticket)
+        addLabel(ticket['key'])
